28a25da93c5c31aeb68eab6189ca9611e67d2cc2
JoeJane <jjose139@my.centennialcollege.ca>
2022-03-03 21:09:50 -0500

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://adminuser:BDZIVUIR32DdRrcG@comp229-f2022-midterm.azcfd.mongodb.net/books229?retryWrites=true&w=majority"
+};
